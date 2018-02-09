cc341fb6de78bf84ae0f2545f78127697f14c296
hungluong249 <hung.luong@matocreative.vn>
2018-02-09 12:18:01 +0700

    building frontend

--- application/views/homepage_view.php
+++ application/views/homepage_view.php
@@ -1,7 +1,31 @@
+<!--
 <div class="content-wrapper">
     <section class="content row">
         <a href="<?php echo base_url('admin/dashboard') ?>">Admin page</a>
         <a href="<?php echo base_url('client/dashboard') ?>">Client page</a>
-<!--        <a href="--><?php //echo base_url('client/user/login') ?><!--">Login as client</a>-->
+        <a href="<?php echo base_url('client/user/login') ?>">Login as client</a>
     </section>
 </div>
+-->
+
+<link rel="stylesheet" href="<?php echo site_url('assets/public/css/homepage.css'); ?>">
+
+<section class="main_content container-fluid">
+    <div class="megatron">
+        <div class="container">
+            <h1>Welcome to the website</h1>
+            <p>n laoreet felis hendrerit magna hendrerit, quis euismod sem sodales. Etiam posuere molestie rhoncus. Curabitur at dui felis. Mauris dignissim sollicitudin est, vel tincidunt odio vestibulum pharetra.</p>
+            <ul class="nav nav-pills nav-justified">
+                <li>
+                    <a href="javascript:void(0);">Admin page</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0);">Client page</a>
+                </li>
+                <li>
+                    <a href="javascript:void(0);">Admin page</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
